778f06797edf07be4935961fc99c2380a68ed205
erguotou <erguotou525@gmail.com>
2022-02-17 16:46:50 +0800

    chore: 🔧 rename React to react

--- src/lib/application/files/React/.lintstagedrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  '**/*.{js,ts,tsx}': [
-    // https://eslint.org/docs/user-guide/command-line-interface
-    'eslint --config ./.eslintrc.cjs --ignore-path ./.eslintignore --fix --color --'
-  ],
-  // https://prettier.io/docs/en/cli.html
-  '*': ['pretty-quick --staged --verbose']
-}
