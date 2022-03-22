b01f05a9ae4a44fcb7ae965ea2c40a5f5537026f
fang <fang@turing>
2022-03-23 00:30:14 +0800

    feat: ✨ update controller

--- src/lib/resource/files/__name__.module.ts
+++ src/lib/resource/files/__name__.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { <%= classify(name) %>Service } from './<%= name %>.service';
-<% if (type === 'rest' || type === 'microservice') { %>import { <%= classify(name) %>Controller } from './<%= name %>.controller';<% } %><% if (type === 'graphql-code-first' || type === 'graphql-schema-first') { %>import { <%= classify(name) %>Resolver } from './<%= name %>.resolver';<% } %><% if (type === 'ws') { %>import { <%= classify(name) %>Gateway } from './<%= name %>.gateway';<% } %>
+<% if (type === 'rest') { %>import { <%= classify(name) %>Controller } from './<%= name %>.controller';<% } %><% if (type === 'graphql-code-first' || type === 'graphql-schema-first') { %>import { <%= classify(name) %>Resolver } from './<%= name %>.resolver';<% } %>
 
 @Module({
-  <% if (type === 'rest' || type === 'microservice') { %>controllers: [<%= classify(name) %>Controller],
+  <% if (type === 'rest') { %>controllers: [<%= classify(name) %>Controller],
   providers: [<%= classify(name) %>Service]<% } else if (type === 'graphql-code-first' || type === 'graphql-schema-first') { %>providers: [<%= classify(name) %>Resolver, <%= classify(name) %>Service]<% } else { %>providers: [<%= classify(name) %>Gateway, <%= classify(name) %>Service]<% } %>
 })
 export class <%= classify(name) %>Module {}
